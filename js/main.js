88fa7de44dfd3807adeaccd975aa872c08068636
Alexandra Cole <acole891@gmail.com>
2026-06-04 05:45:54 +0000

    a work in progress

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ document.getElementById("html-checker").setAttribute("href","https://validator.w
 
 document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
 
+document.getElementById("html-checker-foot").setAttribute("href", "https://validator.w3.org/nu/?doc=" + location.href);
+
+document.getElementById("css-checker-foot").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+
 //manages mobile nav 
 function myFunction() {
     var x = document.getElementById("myTopnav");
