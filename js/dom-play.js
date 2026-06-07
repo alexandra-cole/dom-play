badbc9faef4d9c5707ea236d148aba9b977cc4c9
Alexandra Cole <acole891@gmail.com>
2026-06-06 21:20:53 -0700

    Validator

--- js/dom-play.js
+++ js/dom-play.js
@@ -82,3 +82,9 @@
 
 })();
       
+// Fixes HTML/CSS Checker links
+document.addEventListener("DOMContentLoaded", function() {
+    var currentUrl = window.location.href;
+    document.getElementById("html-checker").href = "https://validator.w3.org/nu/?doc=" + encodeURIComponent(currentUrl);
+    document.getElementById("css-checker").href = "https://jigsaw.w3.org/css-validator/validator?uri=" + encodeURIComponent(currentUrl);
+});
